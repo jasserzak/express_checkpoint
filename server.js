a31360dad0a38c5390de7abbbf56eb575c9b5bce
Jasserzak <jasserslimani01@gmail.com>
2024-05-15 09:44:52 +0100

    updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const workingHours = (req, res, next) => {
   const dayOfWeek = now.getDay();
   const hour = now.getHours();
 
-  if (dayOfWeek >= 1 && dayOfWeek <= 5 && hour >= 10 && hour <= 17) {
+  if (dayOfWeek >= 1 && dayOfWeek <= 5 && hour >= 9 && hour <= 17) {
     next();
   } else {
     res.status(403).send("Out of working hours");
